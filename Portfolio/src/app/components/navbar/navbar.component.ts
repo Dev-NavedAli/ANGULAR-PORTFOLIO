01693cb7cdaf4bc8b4296d124eff6621eaa34343
Naved khan <nomanali50997@gmail.com>
2025-03-01 12:03:26 +0530

    adding functionality to toggling mobile menu

--- Portfolio/src/app/components/navbar/navbar.component.ts
+++ Portfolio/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-navbar',
-  imports: [],
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  menuOpen: boolean = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen; // Toggles between true and false
+    console.log('Menu state:', this.menuOpen); // Debugging log
+  }
 }
