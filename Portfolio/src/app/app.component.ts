7ef767e5d54ef1a0508995bab17125e03a459117
Naved khan <nomanali50997@gmail.com>
2025-03-02 13:29:12 +0530

    debugging

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent,HeaderComponent],
+  imports: [NavbarComponent,HeaderComponent,AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
