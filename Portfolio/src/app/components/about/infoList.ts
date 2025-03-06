157fe6765ac5ffde93126acb64662d77fa2ed8f5
Naved khan <nomanali50997@gmail.com>
2025-03-06 13:16:35 +0530

    rendering images using the third party file

--- Portfolio/src/app/components/about/infoList.ts
+++ Portfolio/src/app/components/about/infoList.ts
@@ -1,5 +1,5 @@
 export const infoList = [
-        { icon: 'assets/code_icon', iconDark: 'assets/code_icon_dark', title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
-        { icon: 'assets/edu_icon', iconDark: 'assets/edu_icon_dark', title: 'Education', description: 'B.Tech in Computer Science' },
-        { icon: 'assets/project_icon', iconDark: 'assets/project_icon_dark', title: 'Projects', description: 'Built more than 5 projects' }
+        { icon: 'assets/code-icon.png', iconDark: 'assets/code-icon-dark.png', title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
+        { icon: 'assets/edu-icon.png', iconDark: 'assets/edu-icon-dark.png', title: 'Education', description: 'B.Tech in Computer Science' },
+        { icon: 'assets/project-icon.png', iconDark: 'assets/project-icon-dark.png', title: 'Projects', description: 'Built more than 5 projects' }
     ];
