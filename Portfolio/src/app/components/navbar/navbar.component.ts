f1c2beed5b90d8118cc6bceefd6a9bc39cbcfdb5
Naved khan <nomanali50997@gmail.com>
2025-03-08 12:47:42 +0530

    learn dynamic class and onwindowscroll

--- Portfolio/src/app/components/navbar/navbar.component.ts
+++ Portfolio/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component , HostListener } from '@angular/core';
+import {NgClass ,CommonModule} from '@angular/common'
 
 @Component({
   selector: 'app-navbar',
+  imports:[NgClass,CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
   menuOpen: boolean = false;
+  isScroll:boolean=false;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen; // Toggles between true and false
     console.log('Menu state:', this.menuOpen); // Debugging log
   }
+
+  @HostListener('window:scroll',[])
+  onWindowScroll(){
+    if(window.scrollY > 50){
+      console.log('user Scrolled by 50 or above');
+      this.isScroll = true;
+    }else{
+      this.isScroll = false;
+    }
+  }
+
+
 }
