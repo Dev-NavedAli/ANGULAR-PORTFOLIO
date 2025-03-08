12200c86994110dc8e329f6ac69588e357f5c703
Naved khan <nomanali50997@gmail.com>
2025-03-08 15:53:07 +0530

    make services

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
 import { ServicesComponent } from './components/services/services.component';
+import { WorkComponent } from './components/work/work.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent,HeaderComponent,AboutComponent, ServicesComponent ],
+  imports: [NavbarComponent,HeaderComponent,AboutComponent, ServicesComponent,WorkComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
