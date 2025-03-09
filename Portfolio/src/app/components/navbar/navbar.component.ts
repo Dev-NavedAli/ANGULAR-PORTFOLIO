af041f1256ebfe68f533e7068a8dc12924528987
Naved khan <nomanali50997@gmail.com>
2025-03-09 14:05:21 +0530

    making the contact component

--- Portfolio/src/app/components/navbar/navbar.component.ts
+++ Portfolio/src/app/components/navbar/navbar.component.ts
@@ -12,7 +12,6 @@ export class NavbarComponent {
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen; // Toggles between true and false
-    console.log('Menu state:', this.menuOpen); // Debugging log
   }
 
   @HostListener('window:scroll',[])
